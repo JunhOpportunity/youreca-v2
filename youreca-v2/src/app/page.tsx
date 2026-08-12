30d9f5fa1f6050781cfe021a31e16eec0ff57236
JunhOpportunity <twinjyjh5@gmail.com>
2026-08-12 21:58:56 +0900

    feat($TanstackQuery) : MSW - Tanstack Query 연결 결과 확인

--- youreca-v2/src/app/page.tsx
+++ youreca-v2/src/app/page.tsx
@@ -1,24 +1,30 @@
 "use client"
 
 import { useEffect } from "react";
+import { useUser } from "../hooks/useUser";
 
 export default function Home() {
 
-  useEffect(() => {
-    const fetchUser = async() => {
-      const response = await fetch("/users/me");
-      const data = response.json();
+  // useEffect(() => {
+  //   const fetchUser = async() => {
+  //     const response = await fetch("/users/me");
+  //     const data = response.json();
 
-      console.log("데이터 : ", data);
-    }
+  //     console.log("데이터 : ", data);
+  //   }
 
-    fetchUser();
+  //   fetchUser();
 
-  }, [])
+  // }, [])
 
-  return (
-    <div>
-      MSW TEST
-    </div>
-  );
+  const { data, isLoading } = useUser();
+
+  if(isLoading) {
+    return <div>LOADING...</div>
+  }
+
+  if (data) {
+    console.log("데이터 : ", data)
+    return <div>Tanstack Query DATA TEST</div>
+  }
 }
