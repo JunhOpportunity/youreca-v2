aec9dbe14d9980fb6bc22eeddc5b58468f3a9ed9
JunhOpportunity <twinjyjh5@gmail.com>
2026-08-20 22:03:54 +0900

    feat($검색) : users 페이지 검색 기능 구현

--- youreca-v2/src/app/users/page.tsx
+++ youreca-v2/src/app/users/page.tsx
@@ -1,10 +1,20 @@
+import SearchUI from "./SearchUI";
+
 export default async function Users({
   searchParams,
 }: {
   searchParams: Promise<{ searchId: string }>;
 }) {
+  const { searchId } = await searchParams;
 
-  const {searchId} = await searchParams;
+  const onSubmit = () => {
+    console.log("검색");
+  };
 
-  return <div>{searchId}</div>;
+  return (
+    <div>
+      {searchId}
+      <SearchUI />
+    </div>
+  );
 }
