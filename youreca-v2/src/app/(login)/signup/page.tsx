d2903e49037bf22ab0e227c915b18c11e0a621fc
JunhOpportunity <twinjyjh5@gmail.com>
2026-08-26 18:22:36 +0900

    feat($auth) : 로그인 및 회원가입 페이지 구현

---
+++ youreca-v2/src/app/(login)/signup/page.tsx
@@ -0,0 +1,11 @@
+import SignupCard from "./SignupCard";
+import SocialLogin from "./SocialLogin";
+
+export default function Signup() {
+  return (
+    <div>
+      <SignupCard />
+      <SocialLogin />
+    </div>
+  );
+}
