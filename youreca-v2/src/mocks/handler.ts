5a363d29daa8d7ba9b8190635de492d5cfa86883
JunhOpportunity <twinjyjh5@gmail.com>
2026-08-26 18:22:08 +0900

    feat($auth) : 로그인 및 회원가입 구현을 위한 모킹 함수 구현

--- youreca-v2/src/mocks/handler.ts
+++ youreca-v2/src/mocks/handler.ts
@@ -2,6 +2,11 @@ import { http, HttpResponse } from "msw";
 import { users } from "./data/users";
 import { reviews } from "./data/reviews";
 
+type LoginRequest = {
+  email: string;
+  password: string;
+};
+
 export const handlers = [
   http.get("/users/me", () => {
     return HttpResponse.json(users[0]);
@@ -12,5 +17,24 @@ export const handlers = [
   http.get("/reviews/1", () => {
     const userReviews = reviews.filter((review) => review.targetUserId === "1");
     return HttpResponse.json(userReviews);
+  }),
+  http.post("/auth/login", async ({ request }) => {
+    const body = (await request.json()) as LoginRequest;
+
+    if (body.email !== "test@test.com" || body.password !== "12345678") {
+      return HttpResponse.json(
+        {
+          message: "이메일 또는 비밀번호가 올바르지 않습니다.",
+        },
+        { status: 401 }
+      );
+    }
+
+    return HttpResponse.json(users[0]);
+  }),
+  http.post("/auth/signup", async ({ request }) => {
+    const body = await request.json();
+
+    return HttpResponse.json(users[0], { status: 201 });
   }),
 ];
