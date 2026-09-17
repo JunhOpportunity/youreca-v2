930205dac33f13ecf4ec8eb5a402e20513f4127f
JunhOpportunity <twinjyjh5@gmail.com>
2026-09-17 21:21:16 +0900

    feat($search) : 검색 결과에 대한 모킹 데이터 구현

---
+++ youreca-v2/src/mocks/data/searchs.ts
@@ -0,0 +1,46 @@
+import { number } from "zod"
+
+export const searchs = [
+  {
+    "id": "1",
+    "nickname": "김철수",
+    "job": "Frontend Developer",
+    "profileImg": "...",
+    "reviewCount": 3,
+    "keywords": [
+      {
+        "name": "팀워크",
+        "count": 3
+      },
+      {
+        "name": "열정",
+        "count": 1
+      },
+      {
+        "name": "책임감",
+        "count": 1
+      }
+    ]
+  }, 
+  {
+    "id": "2",
+    "nickname": "김준호",
+    "job": "Frontend Developer",
+    "profileImg": "...",
+    "reviewCount": 4,
+    "keywords": [
+      {
+        "name": "팀워크",
+        "count": 4
+      },
+      {
+        "name": "열정",
+        "count": 3
+      },
+      {
+        "name": "책임감",
+        "count": 3
+      }
+    ]
+  }
+]
